cb28a14d4417513327e808d4ff7b22b2ea922d37
SoumyosishPal <soumyosishpal.108@gmail.com>
2025-09-19 21:46:32 +0530

    added default message

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.send('AlphaSafari backend is running!');
+  res.send('BengalFusion backend is running!');
 });
 
 // 404 handler
